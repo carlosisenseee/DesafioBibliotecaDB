e653c9f3e7410ba36b80c0b8718e5d3166459260
YAN VICTOR DA SILVA DE MORAIS <yan.morais@alunos.sc.senac.br>
2025-06-13 11:28:45 -0300

    yan

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-    }
-}
+	
+	Scanner scan = new Scanner(System.in);
+    static ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+  //----------//--------------//-----------------//----------------  	
+    	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o seu usuario:");
+        String usuario = scan.next();
+        System.out.println("Digite sua senha");
+       
+        	
+        		System.out.println("""
+					=== Biblioteca Piriri Parara ===
+					Digite para:
+					1 - Emprestar um livro - ok
+					2 - Devolver um livro - ok
+					3 - Alterar Dados - 
+					4 - 
+					
+					
+        			""");
+        		
+        break;
+        
+        }
+    }     		
+				
